63809222c0192136b89997aa4279f9c8cdb485f6
Thomas Carroll <tc.infomatics@gmail.com>
2016-11-01 20:29:18 +0000

    Update newDeploy.sh

--- newDeploy.sh
+++ newDeploy.sh
@@ -18,4 +18,4 @@ cd test
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
